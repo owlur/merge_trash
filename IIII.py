def081d02fa594259363e8733c44838f910dc403
jaewoo <8864248@naver.com>
2018-08-06 01:04:31 +0900

    init

--- IIII.py
+++ IIII.py
@@ -1,19 +1,3 @@
-def llll(lllI):
-    return len(lllI)
-
-def lllI(llII, IIII):
-    llII.append(IIII)
-
-def lIlI(llII):
-    return llII // 2
-
-def lIII():
-    return 0
-
-def lIIl(llII):
-    return llII <= 1
-
-
 def aMM(l):
     return ('',len(l) // 2)
 
