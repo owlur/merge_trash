29fbdaa9cd692abd0eb1f803acbb4a148f9aeda5
jaewoo <8864248@naver.com>
2018-08-06 00:54:50 +0900

    init

--- IIII.py
+++ IIII.py
@@ -15,8 +15,23 @@ def lIIl(llII):
 
 
 def aMM(l):
-    return ('',len(list) // 2)
+    return ('',len(l) // 2)
 
 class cMb:
     def cMP(self):
         pass
+
+def ccccMMM(L,R,alist):
+    i = j = 0
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            alist.append(L[i])
+            i+=1
+        else:
+            alist.append(R[j])
+            j+=1
+    return i,j
+
+def dpcccccIMMMMMMMMMMMMMMM(alist):
+    for _ in range(len(alist)//2):
+        alist.pop(0)
